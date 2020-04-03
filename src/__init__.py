3ae6ff863045f02562b074734788ebdc45bfdfdb
egorpristanskiy <E.Pristanskiy@skoltech.ru>
2020-04-03 21:49:27 +0300

    remove unused files from __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,4 @@
 from .image_form import ImageForm
-from .image_processing import ImageProcessing
-from .image_processing_adapter import ImageProcessingAdapter
 from .login import Login
 from .redirect_to_login import RedirectToLogin
 from .upload_image import UploadImage
